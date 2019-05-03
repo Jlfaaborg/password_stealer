e2a1edebaaf1b14130fe34229bc17cabb834cd48
Joshua Faaborg <joshua.f@PDM.com>
2019-05-03 16:35:37 -0400

    Connected Form To API

--- client/src/presentational/apiCalls.tsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react"
-
-class apiCalls extends React.Component {
-    constructor(props: any) {
-        super(props)
-    }
-}
-
-export default apiCalls
--- client/src/redux/store.tsx
+++ client/src/redux/store.tsx
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-import { createStore } from 'redux';
-
-import reducer from "./reducers";
-
-const store = createStore(reducer, (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
-    (window as any).__REDUX_DEVTOOLS_EXTENSION__());
-=======
 import { createStore, compose, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
 import thunkMiddleware from "redux-thunk";
@@ -14,6 +6,5 @@ import reducer from "./reducers";
 const loggerMiddleware = createLogger();
 
 const store = createStore(reducer, compose(applyMiddleware(thunkMiddleware, loggerMiddleware), (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()))
->>>>>>> first commit
 
 export default store;
